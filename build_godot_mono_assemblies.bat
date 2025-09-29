e46072a718c2c5a86bf4675a76566f6db0374d58
Grifthegnome <grifbd@aol.com>
2025-09-29 14:23:51 -0700

    C# Build Setup - Added NuGet Generation to Build Assemblies Script
    C# Build Setup - Added NuGet Generation to Build Assemblies Script
    1. We now make a local NuGet folder before building assemblies. This is required for local C# development.

--- build_godot_mono_assemblies.bat
+++ build_godot_mono_assemblies.bat
@@ -1,5 +1,15 @@
 @echo off
 cd /d . REM Sets directory to local file dir.
-python ./modules/mono/build_scripts/build_assemblies.py --godot-output-dir=./bin
+
+REM Generate our NuGet directory if we don't have one. 
+IF NOT EXIST "./localNuGet" (
+
+	mkdir "./localNuGet"
+)
+
+REM Tell NuGet to use this directory.
+dotnet nuget add source "./localNuGet" --name MyLocalNugetSource
+
+python ./modules/mono/build_scripts/build_assemblies.py --godot-output-dir ./bin --push-nupkgs-local "./localNuGet"
 
 pause
