ac8e2d7ea766c43f806ecbc06ec75e2167671783
Grifthegnome <grifbd@aol.com>
2025-10-09 09:59:17 -0700

    Build Templates for Godot C# 4.4.2.rc

--- build_godot_mono_export_template_release_4.4.2.rc.bat
+++ build_godot_mono_export_template_release_4.4.2.rc.bat
@@ -4,10 +4,10 @@ set version=4.4.2.rc
 echo building release template for godot %version%
 scons platform=windows target=template_release module_mono_enabled=yes
 
-mkdir %USERPROFILE%\AppData\Roaming\Godot\export_templates\%version%
-rename bin\godot.windows.template_release.x86_64.exe windows_release_x86_64.exe
-rename bin\godot.windows.template_release.x86_64.console.exe windows_release_x86_64_console.exe
-move bin\windows_release_x86_64.exe %USERPROFILE%\AppData\Roaming\Godot\export_templates\%version%
-move bin\windows_release_x86_64_console.exe %USERPROFILE%\AppData\Roaming\Godot\export_templates\%version%
+mkdir %USERPROFILE%\AppData\Roaming\Godot\export_templates\%version%.mono
+rename bin\godot.windows.template_release.x86_64.mono.exe windows_release_x86_64.exe
+rename bin\godot.windows.template_release.x86_64.mono.console.exe windows_release_x86_64_console.exe
+move bin\windows_release_x86_64.exe %USERPROFILE%\AppData\Roaming\Godot\export_templates\%version%.mono
+move bin\windows_release_x86_64_console.exe %USERPROFILE%\AppData\Roaming\Godot\export_templates\%version%.mono
 
 pause
